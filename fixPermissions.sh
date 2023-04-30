7b736e5a2d3412b65fd5cb4a70a26c50259431b6
zavenvar <zaven.vardanyan@gmail.com>
2023-04-29 23:58:20 -0500

    now with parameters -d and -f

--- fixPermissions.sh
+++ fixPermissions.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 
+# Default permissions
+dir_perm=775
+file_perm=644
+
 # Function to change permissions
 change_permissions() {
-    find "$1" -type d -exec chmod 755 {} \; # Change directory permissions to 755
-    find "$1" -type f -exec chmod 644 {} \; # Change file permissions to 644
+    find "$1" -type d -exec chmod $dir_perm {} \; # Change directory permissions
+    find "$1" -type f -exec chmod $file_perm {} \; # Change file permissions
     echo "Permissions for $1 and its content have been updated successfully."
 }
+
+# Parse command line options for directory and file permissions
+while getopts d:f: option
+do
+    case "${option}" in
+        d) dir_perm=${OPTARG};;
+        f) file_perm=${OPTARG};;
+    esac
+done
+shift $((OPTIND -1))
 
 # Check if argument is provided
 if [ $# -eq 0 ]; then
